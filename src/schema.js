3524849e6a7f4898949961c461ac0878ef40e157
Chutiphon.k <chutiphon.k@ku.th>
2017-06-08 01:07:47 +0700

    :tada: Initial commit

---
+++ src/schema.js
@@ -0,0 +1,49 @@
+import TodosList from './data/todos'
+//import data
+
+import {
+  // GraphQL types
+  GraphQLInt,
+  GraphQLFloat,
+  GraphQLString,
+  GraphQLList,
+  GraphQLObjectType,
+  GraphQLEnumType,
+  GraphQLNonNull,
+  GraphQLBoolean,
+
+  //create the schema
+  GraphQLSchema
+} from 'graphql';
+
+const Todo = new GraphQLObjectType({
+  name: "Todo",
+  description: "Todo list",
+  fields: () => ({
+    _id: {type: new GraphQLNonNull(GraphQLString)},
+    title: {type: new GraphQLNonNull(GraphQLString)},
+    completed: {type: GraphQLBoolean}
+  })
+})
+
+
+const Query = new GraphQLObjectType({
+  name: 'TodoSchema',
+  description: 'Root Schema',
+  fields: () => ({
+    todos: {
+      type: new GraphQLList(Todo),
+      resolve: function () {
+        return TodosList
+      }
+    }
+
+  })
+});
+
+// The Schema
+const Schema = new GraphQLSchema({
+  query: Query
+});
+
+export default Schema;
